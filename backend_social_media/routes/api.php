880959482d9336d62474d9fe29e89e3fc21dfbb4
PhamPhien-gif <toilaphien2302@gmail.com>
2025-11-17 22:49:48 +0700

    Update api.php: set rate limit request

--- backend_social_media/routes/api.php
+++ backend_social_media/routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::prefix('auth')->group(function () {
     // Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
-    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+    Route::middleware('throttle:10,1')->post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
     // Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
 });
 
